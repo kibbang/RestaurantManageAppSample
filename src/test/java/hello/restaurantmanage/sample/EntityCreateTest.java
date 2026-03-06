6311a5d24985f8201f5195a0b941d0503fa09b10
kibbang <007kibbang@gmail.com>
2026-03-06 14:21:59 +0900

    테스트파일 및 클로드

---
+++ src/test/java/hello/restaurantmanage/sample/EntityCreateTest.java
@@ -0,0 +1,34 @@
+package hello.restaurantmanage.sample;
+
+
+import hello.restaurantmanage.domain.Restaurant;
+import hello.restaurantmanage.enums.FoodCategory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+public class EntityCreateTest {
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+    void createRestaurant() {
+        Restaurant restaurant = new Restaurant("test1", "test1", FoodCategory.KOREAN);
+        em.persist(restaurant);
+
+        em.flush();
+        em.clear();
+
+        Restaurant findRestaurant = em.find(Restaurant.class, restaurant.getId());
+
+        Assertions.assertThat(findRestaurant.getId()).isEqualTo(restaurant.getId());
+    }
+}
