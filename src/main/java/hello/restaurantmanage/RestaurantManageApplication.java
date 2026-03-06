e2b3d6cb9c98282fa34ef7ed1709f1bbcc7643d9
kibbang <007kibbang@gmail.com>
2026-03-06 11:54:08 +0900

    first commit

---
+++ src/main/java/hello/restaurantmanage/RestaurantManageApplication.java
@@ -0,0 +1,13 @@
+package hello.restaurantmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantManageApplication.class, args);
+    }
+
+}
